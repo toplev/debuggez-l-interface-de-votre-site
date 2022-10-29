f710978ae02d771d2d8d9c17eff168fcbf1212ee
toplev <topourialevan@gmail.com>
2022-10-29 17:32:22 +0200

    update

--- js/pages/signIn.js
+++ js/pages/signIn.js
@@ -1,42 +1,45 @@
-const $signInForm = document.querySelector('.sign-in-form')
+const $signInForm = document.querySelector(".sign-in-form");
 
-const $userEmailInput = document.querySelector('#user-email')
-const $userEmailErrorMsg = document.querySelector('.user-email-error-msg')
+const $userEmailInput = document.querySelector("#user-email");
+const $userEmailErrorMsg = document.querySelector(".user-email-error-msg");
 
-const $userPasswordInput = document.querySelector('#user-password')
-const $userPasswordErrorMsg = document.querySelector('.user-password-error-msg')
+const $userPasswordInput = document.querySelector("#user-password");
+const $userPasswordErrorMsg = document.querySelector(
+  ".user-password-error-msg"
+);
 
-const USER_EMAIL = "thomas@facadia.com"
-const USER_PASSWORD = "azerty"
+const USER_EMAIL = "thomas@facadia.com";
+const USER_PASSWORD = "azerty";
 
 const checkUserEmailInput = () => {
-    const isUserEmailValid = $userEmailInput.value.toLowerCase() === USER_EMAIL
+  const isUserEmailValid = $userEmailInput.value.toLowerCase() === USER_EMAIL;
 
-    if (isUserEmailValid) {
-        $userEmailErrorMsg.classList.add('hidden')
-    } else {
-        $userEmailErrorMsg.classList.remove('hidden')
-    }
+  if (isUserEmailValid) {
+    $userEmailErrorMsg.classList.add("hidden");
+  } else {
+    $userEmailErrorMsg.classList.remove("hidden");
+  }
 
-    return isUserEmailValid
-}
+  return isUserEmailValid;
+};
 
 const checkUserPasswordInput = () => {
-    const isUserPasswordValid = $userPasswordInput.value === USER_PASSWORD
+  const isUserPasswordValid = $userPasswordInput.value === USER_PASSWORD;
 
-    if (isUserPasswordValid) {
-        $userPasswordErrorMsg.classList.add('hidden')
-    } else {
-        $userPasswordErrorMsg.classList.remove('hidden')
-    }
+  if (isUserPasswordValid) {
+    $userPasswordErrorMsg.classList.add("hidden");
+  } else {
+    $userPasswordErrorMsg.classList.remove("hidden");
+  }
 
-    return isUserPasswordValid
-}
+  return isUserPasswordValid;
+};
 
-const isFormValid = () => checkUserEmailInput() && checkUserPasswordInput()
+const isFormValid = () => checkUserEmailInput() && checkUserPasswordInput();
 
-$signInForm.addEventListener('submit', function() {
-    if (isFormValid()) {
-        window.location = 'http://127.0.0.1:5500/pages/homepage.html'
-    }
-})
+$signInForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (isFormValid()) {
+    window.location = "http://127.0.0.1:5500/pages/homepage.html";
+  }
+});
